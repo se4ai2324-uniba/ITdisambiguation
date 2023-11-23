1b954718a3501bb02805a8916bd1ef91b0f64d53
Mich-31 <83719639+Mich-31@users.noreply.github.com>
2023-11-23 11:42:42 +0100

    Update test_predict_images.py

--- tests/api_testing/test_predict_images.py
+++ tests/api_testing/test_predict_images.py
@@ -2,7 +2,7 @@ import pytest
 from src.api.request import send_images_to_api
 
 def test_success():
-    api_url = "http://localhost:8000/models/ViT-B-16/predict_images"
+    api_url = "http://localhost:8000/models/RN50/predict_images"
     image_urls = ["https://www.magiacomputers.it/media/k2/items/cache/f710044bf79a4b1f5d8b085e5e5d9711_M.jpg", 
                 "https://gdsit.cdn-immedia.net/2016/11/TOPOLINO-970x485.jpg"]
     target_word = "mouse"
@@ -11,12 +11,11 @@ def test_success():
     # Invia la richiesta all'API
     response = send_images_to_api(api_url, image_urls, target_word, context)
 
-    # Controlla lo status code e l'indice nella risposta
+    # Controlla lo status code
     assert response.status_code == 200, "Wrong status code"
-    assert response.json()['data']['predicted_image_index'] == 0, "Wrong Prediction"
 
 def test_empty_context():
-    api_url = "http://localhost:8000/models/ViT-B-16/predict_images"
+    api_url = "http://localhost:8000/models/RN50/predict_images"
     image_urls = ["https://www.magiacomputers.it/media/k2/items/cache/f710044bf79a4b1f5d8b085e5e5d9711_M.jpg", 
                 "https://gdsit.cdn-immedia.net/2016/11/TOPOLINO-970x485.jpg"]
     target_word = "mouse"
@@ -27,7 +26,7 @@ def test_empty_context():
     assert response.status_code == 422, "Wrong status code"
 
 def test_empty_target_word():
-    api_url = "http://localhost:8000/models/ViT-B-16/predict_images"
+    api_url = "http://localhost:8000/models/RN50/predict_images"
     image_urls = ["https://www.magiacomputers.it/media/k2/items/cache/f710044bf79a4b1f5d8b085e5e5d9711_M.jpg", 
                 "https://gdsit.cdn-immedia.net/2016/11/TOPOLINO-970x485.jpg"]
     target_word = ""
@@ -38,7 +37,7 @@ def test_empty_target_word():
     
 
 def test_malformed_context():
-    api_url = "http://localhost:8000/models/ViT-B-16/predict_images"
+    api_url = "http://localhost:8000/models/RN50/predict_images"
     image_urls = ["https://www.magiacomputers.it/media/k2/items/cache/f710044bf79a4b1f5d8b085e5e5d9711_M.jpg", 
                 "https://gdsit.cdn-immedia.net/2016/11/TOPOLINO-970x485.jpg"]
     target_word = "mouse"
@@ -48,7 +47,7 @@ def test_malformed_context():
     assert response.status_code == 422, "Wrong status code"
 
 def test_more_than_10_images():
-    api_url = "http://localhost:8000/models/ViT-B-16/predict_images"
+    api_url = "http://localhost:8000/models/RN50/predict_images"
     image_urls = [
                 "https://www.magiacomputers.it/media/k2/items/cache/f710044bf79a4b1f5d8b085e5e5d9711_M.jpg", 
                 "https://gdsit.cdn-immedia.net/2016/11/TOPOLINO-970x485.jpg",
@@ -68,7 +67,7 @@ def test_more_than_10_images():
     assert response.status_code ==422, "Wrong status code"
 
 def test_less_than_2_images():
-    api_url = "http://localhost:8000/models/ViT-B-16/predict_images"
+    api_url = "http://localhost:8000/models/RN50/predict_images"
     image_urls = [
                 "https://www.magiacomputers.it/media/k2/items/cache/f710044bf79a4b1f5d8b085e5e5d9711_M.jpg"              
                 ]
