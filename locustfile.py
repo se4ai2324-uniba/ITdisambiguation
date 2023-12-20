aaf9faa96a0298922f4a44e60e710e5be174767e
Francesco Bottalico <francesco2031@outlook.it>
2023-12-20 13:04:29 +0100

    Updated locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -3,16 +3,18 @@ from urllib.request import urlopen
 from random import choice, sample
 from io import BytesIO
 
-rand_images = [BytesIO(urlopen("https://picsum.photos/224/224").read()) for _ in range(15)]
-
-def get_random_images(n: int = 1):
-    images = sample(rand_images, n)
-    if n == 1:
-        return images[0]
-    return images
-
 class MyUser(HttpUser):
+    host = "https://itdisambiguation.azurewebsites.net"
     wait_time = between(1, 5)
+
+    def on_start(self):
+        self.rand_images = [BytesIO(urlopen("https://picsum.photos/224/224").read()) for _ in range(15)]
+
+    def get_random_images(self, n: int = 1):
+        images = sample(self.rand_images, n)
+        if n == 1:
+            return images[0]
+        return images
 
     @task
     def model_list_task(self):
@@ -26,19 +28,22 @@ class MyUser(HttpUser):
     @task(5)
     def predict_context_task(self):
         model = choice(["RN50", "ViT-B-16"])
-        image = get_random_images(1)
+        image = self.get_random_images(1)
         self.client.post(
             f"/models/{model}/predict_context",
             files={"image": image},
-            data={"target_word": "test_word", "contexts": "context1, context2"}
+            data={"target_word": "test_target", "contexts": "test_context1, test_context2"}
         )
+        image.seek(0)
 
     @task(5)
     def predict_images_task(self):
         model = choice(["RN50", "ViT-B-16"])
-        images = get_random_images(4)
+        images = self.get_random_images(4)
         self.client.post(
             f"/models/{model}/predict_images",
             files=[("images", x) for x in images],
-            data={"target_word": "test_word", "context": "context_word"}
+            data={"target_word": "test_target", "context": "test_context"}
         )
+        for i in images:
+            i.seek(0)
