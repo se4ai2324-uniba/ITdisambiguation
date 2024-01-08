bcc903f7c5a22d0ccec1afc0332e4ebdacdd5726
Mich-31 <mikelediki22@gmail.com>
2024-01-08 13:18:49 +0100

    Fixed errors

--- src/api/api.py
+++ src/api/api.py
@@ -14,8 +14,8 @@ from fastapi import FastAPI, HTTPException, status, File, UploadFile, Depends, F
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.encoders import jsonable_encoder
 from ..conf import config
-from api.schemas import PredictContextPayload, PredictImagesPayload, GetModelNamesResponseModel, GetModelNamesData, GetModelInfosResponseModel, ModelMetrics, GetModelInfosData, PredictContextResponseModel, PredictContextResponseData, PredictImageResponseModel, PredictImageResponseData
-from api.prometheus.instrumentator import instrumentator
+from schemas import PredictContextPayload, PredictImagesPayload, GetModelNamesResponseModel, GetModelNamesData, GetModelInfosResponseModel, ModelMetrics, GetModelInfosData, PredictContextResponseModel, PredictContextResponseData, PredictImageResponseModel, PredictImageResponseData
+from prometheus.instrumentator import instrumentator
 from ..models.evaluate import predict_context, predict
 sys.path.append("src")
 
